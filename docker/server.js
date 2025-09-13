410201ec556ae87dfc2edc67dcb00d2a77a7fca9
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-09-13 11:17:43 +0530

    doc

---
+++ docker/server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+
+    res.status(200).json({
+        message:'Hello from Docker container!'
+    });
+});
+
+
+app.listen(3000,()=>{
+
+    console.log('Server is running on port 3000');
+
+});
