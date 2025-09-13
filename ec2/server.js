554f0f394638cee5577f6cdf3f22f63061d0ae5d
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-09-13 10:55:38 +0530

    first commit

---
+++ ec2/server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+
+    res.json({
+        message:'Hello from EC2 instance!'
+    });
+});
+
+
+app.listen(3000,()=>{
+
+    console.log('Server is running on port 3000');
+
+});
